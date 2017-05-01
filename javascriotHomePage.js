7b9e1294f581015598f2c96394b4ca92a0372023
Suliman Sharif <sharifsuliman1@gmail.com>
2017-05-01 00:47:15 -0500

    Re did the about page and the project page, still need to fix the scrolling

--- javascriotHomePage.js
+++ javascriotHomePage.js
@@ -88,3 +88,11 @@ $(function() {
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
+
+
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+});
